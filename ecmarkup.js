2073e0e902d8dec4d41856c6911c7b353e907cf4
Brian Terlson <brian.terlson@microsoft.com>
2015-10-14 20:31:17 -0700

    Update gh-pages

--- ecmarkup.js
+++ ecmarkup.js
@@ -34,3 +34,51 @@ function init() {
 }
 
 document.addEventListener('DOMContentLoaded', init);
+function findLocalReferences ($elem) {
+  var name = $elem.innerHTML;
+  var parentAlg = $elem.parentNode;
+  var references = [];
+
+  while (parentAlg && parentAlg.nodeName !== 'EMU-ALG') {
+    parentAlg = parentAlg.parentNode;
+  }
+
+  if(!parentAlg) return;
+
+  var vars = parentAlg.querySelectorAll('var');
+
+  for (var i = 0; i < vars.length; i++) {
+    var $var = vars[i];
+
+    if ($var.innerHTML === name) {
+      references.push($var);
+    }
+  }
+
+  return references;
+}
+
+function toggleFindLocalReferences($elem) {
+  var references = findLocalReferences($elem);
+  if ($elem.classList.contains('referenced')) {
+    references.forEach(function ($reference) {
+      $reference.classList.remove('referenced');
+    });
+  } else {
+    references.forEach(function ($reference) {
+      $reference.classList.add('referenced');
+    });
+  }
+}
+
+function installFindLocalReferences () {
+  var vars = document.querySelectorAll('emu-alg var');
+
+  for (var i = 0; i < vars.length; i++) {
+    var $var = vars[i];
+
+    $var.addEventListener('click', toggleFindLocalReferences.bind(null, $var));
+  }
+}
+
+document.addEventListener('DOMContentLoaded', installFindLocalReferences);
